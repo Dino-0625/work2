59f36ee163ea1b3f00ac1631dddf38439d936621
dino <dinolee0625xd@gmil.com>
2020-10-03 01:26:39 +0800

    first time use commit

---
+++ work2/second.swift
@@ -0,0 +1,20 @@
+//
+//  second.swift
+//  work2
+//
+//  Created by tsui erh li on 2020/10/2.
+//
+
+import SwiftUI
+
+struct secondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct second_Previews: PreviewProvider {
+    static var previews: some View {
+        secondView()
+    }
+}
